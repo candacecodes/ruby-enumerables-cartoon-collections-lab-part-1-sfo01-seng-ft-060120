5268ffb962784f8b09be6e37892b96656f6e692b
eager-class-5814 <candacehchoe@gmail.com>
2020-05-20 20:55:23 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,11 +1,22 @@
 def greet_characters(array)
   # Use `each` to enumerate over the provided array
-  #
   # Print a custom greeting for each element
+  characters = array 
+  total = 0 
+  characters.each do | greeting | 
+    puts "Hello #{characters[total]}!"
+    total += 1 
 end
 
 def list_dwarves(array)
   # Use `each_with_index` to enumerate over the provided array
-  #
   # Print a numbered list of each element
-end
+  characters = array 
+  total = 0 
+  index = 1 
+  characters.each do | list | 
+    puts "#[index]. {characters[total]}"
+  end 
+    total += 1 
+    index += 1 
+  end 
