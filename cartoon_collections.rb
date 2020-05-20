2bbc579e5b76c89ae896d199c91391c8156a45b9
eager-class-5814 <candacehchoe@gmail.com>
2020-05-20 22:01:50 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,7 +14,7 @@ def list_dwarves(array)
   characters = array 
   total = 0 
   index = 0 
-  (characters).each_with_index {|characters, index | puts "#{index+1} #{characters}""
+  (characters).each_with_index {|characters, index | puts "#{index+1} #{characters[total]}"}
 end 
   total += 1 
 end 
